38aeb0a48028805f8701e9db1f05b3610efe1273
Alan G. Isaac <alan.isaac@gmail.com>
2023-01-14 15:14:33 -0500

    increment required python and docutils versions

--- setup.py
+++ setup.py
@@ -1,37 +1,49 @@
-from setuptools import setup, find_packages
-import sys, os
+#!/usr/bin/env python
 
-from rst2beamer import __version__
+"""Installer for rst2beamer."""
+
+import ast
+
+from setuptools import setup
+
+
+def version():
+    """Return version string."""
+    with open('rst2beamer.py') as input_file:
+        for line in input_file:
+            if line.startswith('__version__'):
+                return ast.parse(line).body[0].value.s
+
+with open('README.rst') as readme:
+    with open('HISTORY.rst') as history:
+        DESCRIPTION = readme.read() + '\n' + history.read()
+
 
 setup(
-	name='rst2beamer',
-	version=__version__,
-	description="A docutils writer and script for converting restructured text to the Beamer presentation format",
-	long_description=open("README.txt").read() + "\n" +
-		open(os.path.join("docs", "HISTORY.txt")).read(),
-	classifiers=[
-		'Development Status :: 5 - Production/Stable',
-		'License :: OSI Approved :: GNU General Public License (GPL)',
-		'Programming Language :: Python',
-		'Topic :: Text Processing :: Markup',
-		'Topic :: Utilities',
-		'Topic :: Multimedia :: Graphics :: Presentation',
-	],
-	keywords='presentation docutils rst restructured-text',
-	author='Ryan Krauss & Paul-Michael Agapow',
-	author_email='ryanwkrauss@gmail.com, agapow@bbsrc.ac.uk',
-	url='http://www.agapow.net/software/rst2beamer',
-	license='GPL',
-	#packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
-	py_modules=['rst2beamer'],
-	#include_package_data=True,
-	zip_safe=False,
-	install_requires=[
-		'docutils',
-	],
-	entry_points={
-		'console_scripts': [
-			'rst2beamer = rst2beamer:main',
-		],
-	},
+    name='rst2beamer3k',
+    version=version(),
+    description='A docutils writer and script for converting restructured '
+                'text to the Beamer presentation format',
+    long_description=DESCRIPTION,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Programming Language :: Python :: 3',
+        'Topic :: Text Processing :: Markup',
+        'Topic :: Utilities',
+        'Topic :: Multimedia :: Graphics :: Presentation',
+    ],
+    keywords='presentation docutils rst restructured-text',
+    url='https://github.com/myint/rst2beamer',
+    license='GPL',
+    py_modules=['rst2beamer'],
+    zip_safe=False,
+    install_requires=[
+        'docutils >= 0.19',
+    ],
+    entry_points={
+        'console_scripts': [
+            'rst2beamer = rst2beamer:main',
+        ]
+    }
 )
